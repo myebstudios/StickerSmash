16232d7b84bee49ec9c871068963f2494941484a
Esoe B.Studios <myebstudios@gmail.com>
2024-04-13 10:12:04 +0100

    Displayed the selected emoji

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -53,6 +54,7 @@ export default function App() {
 
       <View style={styles.imageContainer}>
           <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
       </View>
 
           {showAppOptions ? (
